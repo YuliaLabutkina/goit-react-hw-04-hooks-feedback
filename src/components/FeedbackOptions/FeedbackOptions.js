18364f1cdac051d4198dfe8e61e530a71fa10e40
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-12-14 17:19:18 +0200

    added hw-04-hooks-feedback

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map((option, id) => (
+    <button className={s.button} key={id} onClick={onLeaveFeedback}>
+      {option}
+    </button>
+  ));
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
